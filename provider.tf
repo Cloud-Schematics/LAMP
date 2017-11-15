811e301e970854d50130e3119ce36a5e1b1bb796
Kate Lyons <knlyons@us.ibm.com>
2017-11-15 09:42:54 -0600

    Updated for rebranding.

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 variable "softlayer_username" {
-  description = "Your Bluemix Infrastructure (SoftLayer) user name."
+  description = "Your IBM Cloud Infrastructure (SoftLayer) user name."
 }
 variable "softlayer_api_key" {
-  description = "Your Bluemix Infrastructure (SoftLayer) API key."
+  description = "Your IBM Cloud Infrastructure (SoftLayer) API key."
 }
 
 provider "ibm" {
