fd73df07c910453eafce425c3aa37dfb3c194ffe
hlucey <42588404+hlucey@users.noreply.github.com>
2019-09-17 10:29:32 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 variable "softlayer_username" {
-  description = "Enter your classic infrastructure (SoftLayer) full user name. Go to https://cloud.ibm.com/iam/users/, select your name, and get your user name from the VPN password section."
+  description = "Enter your classic infrastructure user name. Go to https://cloud.ibm.com/iam/users/, select your name, and get your user name from the VPN password section."
 }
 variable "softlayer_api_key" {
   description = "Enter your classic infrastructure API key. Go to https://cloud.ibm.com/iam/apikeys to get the details of an existing API key or create a new one."
