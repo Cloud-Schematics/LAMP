95d08ba38e85fdaa5baef7a9ae36ab26ea7f4bf6
hlucey <42588404+hlucey@users.noreply.github.com>
2019-09-17 10:20:21 -0400

    Updates to the deployment values
    We would like our editor @cdmauney to review the strings prior to merge.

--- vm.tf
+++ vm.tf
@@ -14,19 +14,19 @@
 ################################################################
 variable "hostname" {
   default = "lamp"
-  description = "The hostname for the instance."
+  description = "The hostname for the instance. For example: vm1"
 }
 variable "domain" {
   default = "domain.dev"
-  description = "The domain for the instance."
+  description = "The domain for the instance. For example: domain.dev"
 }
 variable "datacenter" {
   default = "wdc01"
-  description = "The data center to create resources in."
+  description = "The data center to create resources in, for example, dal13. To get a list of all data centers, run the "ic sl vs options" command."
 }
 variable "os_reference_code" {
   default = "CENTOS_LATEST_64"
-  description = "The operating system reference code used to provision the computing instance."
+  description = "The operating system reference code used to provision the computing instance. For example: CENTOS_7_64"
 }
 variable "cores" {
   default = "1"
@@ -34,28 +34,28 @@ variable "cores" {
 }
 variable "memory" {
   default = "1024"
-  description = "The amount of memory (in Mb) to allocate."
+  description = "The amount of memory in MB to allocate."
 }
 variable "disk_size" {
   default = "25"
-  description = "The numeric disk sizes (in GB) for the instance’s block device and disk image settings."
+  description = "The numeric disk sizes in GB for the instance’s block device and disk image settings."
 }
 variable "private_network_only" {
   default = "false"
-  description = "When set to true, a compute instance only has access to the private network."
+  description = "Enter true or false. True means a compute instance has access only to the private network."
 }
 variable "network_speed" {
   default = "100"
-  description = "The connection speed (in Mbps) for the instance’s network components."
+  description = "The connection speed in Mbps for the instance’s network components."
 }
 variable "tags" {
   default = "lamp"
-  description = "Descriptive tags to label the resource."
+  description = "Add descriptive tags to label the resource.  A tag is a label that you assign to a resource for easy filtering of resources in your resource list."
 }
 
 variable "ssh_user" {
   default = "root"
-  description = "The default username for the VM."
+  description = "The default user name for the virtual machine. For example, the "root" user for UNIX operating systems."
 }
 variable "ssh_label" {
   default = "public ssh key"
@@ -67,7 +67,7 @@ variable "ssh_notes" {
 }
 variable "ssh_key" {
   default = ""
-  description = "Your public SSH key to access the VM."
+  description = "Your public SSH key to access the virtual machine. Your public key is saved to a file called id_rsa.pub in the .ssh subdirectory of your home directory."
 }
 
 resource "ibm_compute_ssh_key" "ssh_key" {
